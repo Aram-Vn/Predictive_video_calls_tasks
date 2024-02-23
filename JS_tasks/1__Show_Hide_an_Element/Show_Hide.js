2d208d9204e829d294d4b094ed7ccee9fe53d418
Aram-Vn <aramvardanyan13@gmail.com>
2024-02-23 17:13:16 +0400

    finish show/hide

--- JS_tasks/1__Show_Hide_an_Element/Show_Hide.js
+++ JS_tasks/1__Show_Hide_an_Element/Show_Hide.js
@@ -1,12 +1,11 @@
 function show_hide() {
-  let p = document.getElementById('text')
-  let button = document.getElementById('hide')
-  if (p.style.display === 'none') {
-    p.style.display = 'block';
+  let myDiv = document.getElementById('myDiv');
+  let button = document.getElementById('hide');
+  if (myDiv.style.display === 'none') {
+    myDiv.style.display = 'block';
     button.innerHTML = 'Hide';
-  }
-  else {
-    p.style.display = 'none';
-    button.innerHTML = 'show';
+  } else {
+    myDiv.style.display = 'none';
+    button.innerHTML = 'Show';
   }
 }
